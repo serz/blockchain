8753ea7a16a492b50f748d9f270f562d253ca2ab
Sergei Kalinistov <sergei.kalinistov@paxful.com>
2020-04-08 16:46:27 +0300

    api endpoints

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const GENESIS_DATA = {
     timestamp: 1586163056,
     lastHash: '-',
     hash: '76b0ed962e05cde1b28f1a0fb8755a2c',
-    data: [],
+    data: "genesis",
     difficulty: INITIAL_DIFFICULTY,
     nonce: 0,
 };
